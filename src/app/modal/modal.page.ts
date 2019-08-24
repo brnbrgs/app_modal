179ae401d71daf43d1561c39bab4fa0968f8b5b6
brnbrgs <theneighbourges@gmail.com>
2019-08-24 11:46:11 -0300

    Término da função de IMC

--- src/app/modal/modal.page.ts
+++ src/app/modal/modal.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-modal',
@@ -11,12 +12,13 @@ export class ModalPage implements OnInit {
   peso:any;
   altura:any;
   imc:any;
+  response:any;
 
-  constructor(public modalController: ModalController) {}
+  constructor(public modalController: ModalController, public alertController: AlertController) {}
 
   ngOnInit() {
   }
-
+  
   async closeModal() {
     const onClosedData:string = "Fechou!";
     await this.modalController.dismiss(onClosedData);
@@ -25,7 +27,34 @@ export class ModalPage implements OnInit {
   async enviaDados(){
 
     this.imc = this.peso / (this.altura * this.altura);
-    console.log(this.imc);
+
+    if(this.imc <= 20) {
+      this.response = 'baixo peso';
+    } else if(this.imc >= 20 && this.imc <= 24) {
+      this.response = 'peso normal';
+    } else if(this.imc >= 24.1 && this.imc <= 29) {
+      this.response = 'sobrepeso';
+    } else if(this.imc >= 29.1 && this.imc <= 34.9) {
+      this.response = 'obesidade';
+    } else if(this.imc >= 35) {
+      this.response = 'obesidade mórbida';
+    } 
+    
+    const alert = await this.alertController.create({
+      header: 'Resultado',
+      subHeader: 'Seu resultado de IMC foi de ' + this.imc.toFixed(2),
+      message: 'Isso significa que você está com ' + this.response,
+      buttons: [
+        {
+          text: 'Ok',
+          handler: () => {
+            this.closeModal();
+          }
+        }
+      ]
+    });
+
+    await alert.present(); 
 
   }
 
